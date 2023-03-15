c174216ab40d02ee67447d5e45beb0e71104b80d
Mehmet Alpkaya <alpkaya71@gmail.com>
2023-03-15 15:39:15 +0300

    Api error

--- src/main/java/com/LayShare/LayShare/error/ApiError.java
+++ src/main/java/com/LayShare/LayShare/error/ApiError.java
@@ -7,17 +7,17 @@ import java.util.Map;
 
 @Data
 public class ApiError {
-    private int status;
+    private int status; // hata kodu
 
-    private String message;
+    private String message; // hatayla ilgili mesaj
 
-    private  String path;
+    private  String path; // hatanın hangi path da olduğu
 
-    private  long timestap= new Date().getTime();
+    private  long timestap= new Date().getTime(); // hatanın zamanı ms cinsinden tutar
 
-    private Map<String,String> validationErrorss;
-
-    public  ApiError (int status, String message, String path)
+    private Map<String,String> validationErrorss; // validation hatası key string değeri string bir map tutarız
+// json olarak tutulan bir response da tutulacak bu yüzden getter setterlar kullanıyoruz
+    public  ApiError (int status, String message, String path) // constructer
     {
         this.status=status;
         this.message=message;
