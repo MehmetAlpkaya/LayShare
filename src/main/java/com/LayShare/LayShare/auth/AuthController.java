71962d121feb0773f3cf24df839ae00da9406a75
Mehmet Alpkaya <alpkaya71@gmail.com>
2023-04-20 16:37:33 +0300

    AuthController

--- src/main/java/com/LayShare/LayShare/auth/AuthController.java
+++ src/main/java/com/LayShare/LayShare/auth/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController
 {
-    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);//55
     @PostMapping("/api/1.0/auth")
     ResponseEntity<?> handleAuothentication(@RequestHeader(name="Authorization", required = false) String authorization )
     {
